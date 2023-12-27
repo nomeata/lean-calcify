630213d3641c1b3aff7cb68a0a11add25d36fd8c
Joachim Breitner <mail@joachim-breitner.de>
2023-12-27 11:07:20 +0100

    Stash

---
+++ Calcify.lean
@@ -0,0 +1,3 @@
+-- This module serves as the root of the `Calcify` library.
+-- Import modules here that should be built as part of the library.
+import «Calcify».Basic
---
+++ lakefile.lean
@@ -0,0 +1,18 @@
+import Lake
+open Lake DSL
+
+require std from git "https://github.com/leanprover/std4" @ "main"
+
+package «calcify» where
+  -- add package configuration options here
+
+lean_lib «Calcify» where
+  -- add library configuration options here
+
+@[default_target]
+lean_exe «calcify» where
+  root := `Main
+  -- Enables the use of the Lean interpreter by the executable (e.g.,
+  -- `runFrontend`) at the expense of increased binary size on Linux.
+  -- Remove this line if you do not need such functionality.
+  supportInterpreter := true
