c8fa7bdf1e55a7b2ca6e81e7d18d2b22b3160b46
Joachim Breitner <mail@joachim-breitner.de>
2024-03-01 16:34:26 +0100

    Remove Main

--- lakefile.lean
+++ lakefile.lean
@@ -6,13 +6,6 @@ open Lake DSL
 package «calcify» where
   -- add package configuration options here
 
+@[default_target]
 lean_lib «Calcify» where
   -- add library configuration options here
-
-@[default_target]
-lean_exe «calcify» where
-  root := `Main
-  -- Enables the use of the Lean interpreter by the executable (e.g.,
-  -- `runFrontend`) at the expense of increased binary size on Linux.
-  -- Remove this line if you do not need such functionality.
-  supportInterpreter := true
