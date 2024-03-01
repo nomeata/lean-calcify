400115e3cea7fcc7ca454c46e2d59046b0b0c61c
Joachim Breitner <mail@joachim-breitner.de>
2024-03-01 16:32:07 +0100

    Cleanup

--- Calcify/Basic.lean
+++ Calcify/Basic.lean
@@ -8,19 +8,37 @@ open Lean Elab Tactic Meta
 
 open Lean.Meta.Tactic.TryThis (delabToRefinableSyntax addSuggestion)
 
-def mkCongrArg' (f p : Expr) : MetaM Expr := do
-  if let .lam _ _ b _ := f then
-    if ! b.hasLooseBVars then
-      return ← mkEqRefl b
-  if let .lam _ _ (.bvar 0) _ := f then
-    return p
-  mkCongrArg f p
-
 partial def mkEqTrans' (p₁ p₂ : Expr) : MetaM Expr := do
   if let mkApp6 (.const ``Eq.trans _) _ _ _ _ p₁₁ p₁₂ := p₁ then
     mkEqTrans' p₁₁ (← mkEqTrans' p₁₂ p₂)
   else
     mkEqTrans p₁ p₂
+
+
+def mkCongrArg' (f p : Expr) : MetaM Expr := do
+  -- The function is constant? This becomes refl
+  if let .lam _ _ b _ := f then
+    if ! b.hasLooseBVars then
+      return ← mkEqRefl b
+
+  -- The function is the identity? Short-circuit
+  if let .lam _ _ (.bvar 0) _ := f then
+    return p
+
+  -- Push congr into transitivity
+  if let mkApp6 (.const ``Eq.trans _) _ _ _ _ p1 p2 := p then
+    return ← mkEqTrans' (← mkCongrArg' f p1) (← mkCongrArg' f p2)
+
+  mkCongrArg f p
+
+-- congrFun is a special case of congrArg
+def mkCongrFun' (h x : Expr) : MetaM Expr := do
+  let some (α, _f₁, _f₂) := (← inferType h).eq? | throwError "Expected proof of equality"
+  mkCongrArg' (.lam "f" α (.app (.bvar 0) x) .default) h
+
+-- congr can be written as a composition of congrFun and congrArg
+def mkCongr' (x₁ f₂ : Expr) (p1 p2 : Expr) : MetaM Expr := do
+    mkEqTrans' (← mkCongrFun' p1 x₁) (← mkCongrArg' f₂ p2)
 
 partial def mkEqMPR' (e1 e2 : Expr) : MetaM Expr := do
   -- A mpr applied to an congruence with equality can be turned into transitivities
@@ -40,7 +58,6 @@ partial def mkEqMPR' (e1 e2 : Expr) : MetaM Expr := do
 
   mkEqMPR e1 e2
 
-
 partial def mkOfEqTrue' : Expr → MetaM Expr
   | mkApp2 (.const ``eq_self _) _α a
   => mkEqRefl a
@@ -48,78 +65,37 @@ partial def mkOfEqTrue' : Expr → MetaM Expr
   | mkApp2 (.const ``eq_true []) _P p
   => pure p
 
-
   | mkApp6 (.const ``Eq.trans [_u]) _ _ _ _ p1 p2
   => do mkEqMPR' p1 (← mkOfEqTrue' p2)
 
   | p
   => do mkOfEqTrue p
 
-
-/--
-Takes a proof of `(a = b) = (a' = b')` and returns a proof of `a = a'` and `b' = b`.
--/
-def split_eq_true : Expr → MetaM (Expr × Expr × Expr × Expr × Expr × Expr)
-  | mkApp6 (.const ``congrFun [u, _v]) β _
-      (mkApp2 (.const ``Eq _) _α a)
-      (mkApp2 (.const ``Eq _) _α2 a')
-      (mkApp6 (.const ``congrArg _) _α3 _ _a _a' (.app (.const ``Eq _) _) p1)
-      b
-  => return (a, p1, a', b, mkApp2 (.const ``Eq.refl [u]) β b, b)
-  | mkApp6 (.const ``congrArg [u, v]) β _
-      b b'
-      (mkApp2 (.const ``Eq _) α a)
-      p2
-  => return (a, mkApp2 (.const ``Eq.refl [v]) α a, a,
-             b', mkApp4 (.const ``Eq.symm [u]) β b b' p2, b)
-  | mkApp6 (.const ``congrArg [_u, _v]) _α _
-      a a'
-      (.lam _ _ (mkApp3 (.const ``Eq [v]) β (.bvar 0) b) _)
-      p2
-  => return (a, p2, a',
-             b, mkApp2 (.const ``Eq.refl [v]) β b, b)
-  | mkApp8 (.const ``congr [u, _v]) β (.sort 0)
-      (mkApp2 (.const ``Eq _) _α a)
-      (mkApp2 (.const ``Eq _) _α2 a')
-      b b'
-      (mkApp6 (.const ``congrArg _) _α3 _ _a _a' (.app (.const ``Eq _) _) p1)
-      p2
-  => return (a, p1, a', b', mkApp4 (.const ``Eq.symm [u]) β b b' p2, b)
-  | e
-  => throwError m!"Expected proof of `(a = b) = (a' = b')`, but got:\n{e}"
-
-partial def simplify' : Expr → MetaM Expr
-  | mkApp2 (.const ``of_eq_true _) _ p
-  => do mkOfEqTrue' (← simplify' p)
+partial def simplify : Expr → MetaM Expr
 
   -- eliminate id application, and hope for the best
-  | mkApp4 (.const ``Eq.mpr us) α β (mkApp2 (.const ``id _) _ p) p2
-  => simplify' (mkApp4 (.const ``Eq.mpr us) α β p p2)
+  | mkApp2 (.const ``id _) _ p
+  => simplify p
 
-  -- replace congrFun with congrArg
-  | mkApp6 (.const ``congrFun [_u, _v]) _ _ f₁ _ p1 x
-  => do simplify' (← mkCongrArg (.lam "f" (←inferType f₁) (.app (.bvar 0) x) .default) p1)
+  -- Use the smart constructors above
 
-  -- take congr apart
   | mkApp8 (.const ``congr [_u, _v]) _α _β _f₁ f₂ x₁ _x₂ p1 p2
-  => do
-    let eqf ← simplify' (← mkCongrFun (← simplify' p1) x₁)
-    let eqx ← simplify' (← mkCongrArg f₂ (← simplify' p2))
-    mkEqTrans' eqf eqx
+  => do mkCongr' x₁ f₂ (← simplify p1) (← simplify p2)
 
-  -- push congrArg into trans
-  | mkApp6 (.const ``congrArg [_u, _v]) _α _β _a _a' f
-    (mkApp6 (.const ``Eq.trans _) _ _ _ _ p1 p2)
-  => do simplify' (← mkEqTrans' (← mkCongrArg f p1) (← mkCongrArg f p2))
+  | mkApp2 (.const ``of_eq_true _) _ p
+  => do mkOfEqTrue' (← simplify p)
 
-  -- simplify under mpr
+  | mkApp6 (.const ``congrFun [_u, _v]) _ _ _ _ p1 x
+  => do mkCongrFun' (← simplify p1) x
+
+  | mkApp6 (.const ``congrArg [_u, _v]) _α _β _a _a' f p
+  => do mkCongrArg' f (← simplify p)
+
   | mkApp4 (.const ``Eq.mpr _) _ _ p₁ p₂
-  => do
-    mkEqMPR' (← simplify' p₁) (← simplify' p₂)
+  => do mkEqMPR' (← simplify p₁) (← simplify p₂)
 
-  -- simplify under trans
   | mkApp6 (.const ``Eq.trans _) _α _a _b _c p1 p2
-  => do mkEqTrans' (← simplify' p1) (← simplify' p2)
+  => do mkEqTrans' (← simplify p1) (← simplify p2)
 
   | e => pure e
 
@@ -163,7 +139,7 @@ elab (name := calcifyTac) tk:"calcify " t:tacticSeq : tactic => withMainContext 
   let goalMVar ← getMainGoal
   evalTactic t
   let proof ← instantiateMVars (mkMVar goalMVar)
-  let proof ← simplify' proof
+  let proof ← simplify proof
   check proof
   -- let proofStx ← delabToRefinableSyntax proof
   let tactic ← delabProof proof
